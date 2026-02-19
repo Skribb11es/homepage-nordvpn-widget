6d6d69cff913830cc0f5a70c4b8bf69bea69c858
Skribb11es <wd186899@gmail.com>
2026-02-19 03:16:29 -0600

    Update dependencies

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -euo pipefail
 
 REPO_URL="https://github.com/Skribb11es/homepage-nordvpn-widget"
@@ -13,15 +14,14 @@ if [[ "$EUID" -ne 0 ]]; then
   exit 1
 fi
 
-echo "[1/7] installing python..."
-apt update
+echo "[1/6] installing python..."
+apt update && apt upgrade -y
 apt install -y git python3
-
-echo "[2/7] installing Flask..."
-apt install python3-flask
+apt install -y python3-flask
+apt install -y python3-gunicorn
 
 TMP_DIR="$(mktemp -d)"
-echo "[3/7] cloning into $TMP_DIR ..."
+echo "[2/6] cloning into $TMP_DIR ..."
 git clone "$REPO_URL" "$TMP_DIR"
 
 if [[ ! -f "$TMP_DIR/server.py" ]]; then
@@ -34,12 +34,12 @@ if [[ ! -f "$TMP_DIR/$SERVICE_FILE_SRC" ]]; then
   exit 1
 fi
 
-echo "[4/7] moving server.py into $INSTALL_DIR ..."
+echo "[3/6] moving server.py into $INSTALL_DIR ..."
 mkdir -p "$INSTALL_DIR"
 cp -f "$TMP_DIR/server.py" "$INSTALL_DIR/server.py"
 chmod +x "$INSTALL_DIR/server.py"
 
-echo "[5/7] creating env file in $ENV_FILE ..."
+echo "[4/6] creating env file in $ENV_FILE ..."
 if [[ ! -f "$ENV_FILE" ]]; then
   cat > "$ENV_FILE" <<EOF
 EOF
@@ -49,7 +49,7 @@ else
   echo "$ENV_FILE already exists."
 fi
 
-echo "[6/7] setting up systemd service..."
+echo "[5/6] setting up systemd service..."
 cp -f "$TMP_DIR/$SERVICE_FILE_SRC" "$SERVICE_FILE_DST"
 
 systemctl daemon-reload
@@ -57,7 +57,7 @@ systemctl enable nord-status-server.service
 
 rm -rf "$TMP_DIR"
 
-echo "[7/7] finished!"
+echo "[6/6] finished!"
 echo
 echo "Service status:"
 systemctl status nord-status-server.service --no-pager || true
